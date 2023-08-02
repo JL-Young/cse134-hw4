73ab5242f3dc507edbd8f28ab4e5214d49968264
JL-Young <jlyoung@ucsd.edu>
2023-08-01 18:35:47 -0700

    1a, 2, 3, 4, 5a completed
    1a, 2, 3, 4, 5a completed
    1b, 5b missing

--- dom.js
+++ dom.js
@@ -16,14 +16,44 @@ function init() {
         modify();
     });
 
+    element = document.getElementById('advModifyBtn');
+    element.addEventListener('click', function () {
+        advModify();
+    });
+
     element = document.getElementById('addBtn');
     element.addEventListener('click', function () {
         add();
     });
 
+    element = document.getElementById('advAddBtn');
+    element.addEventListener('click', function () {
+        advAdd();
+    });
+
     element = document.getElementById('removeBtn');
     element.addEventListener('click', function () {
         remove();
+    });
+
+    element = document.getElementById('safeRmvBtn');
+    element.addEventListener('click', function () {
+        safeRemove();
+    });
+
+    element = document.getElementById('selectRmvBtn');
+    element.addEventListener('click', function () {
+        selectiveRemove();
+    });
+
+    element = document.getElementById('cloneBtn');
+    element.addEventListener('click', function () {
+        clone();
+    });
+    
+    element = document.getElementById('advCloneBtn');
+    element.addEventListener('click', function () {
+        advClone();
     });
 }
 
@@ -35,32 +65,41 @@ function walk() {
  
     el = el.firstChild;
     showNode(el);
- 
+
     el = el.nextSibling;
     showNode(el);
- 
+
     el = el.lastChild;
     showNode(el);
- 
+
     el = el.parentNode.parentNode.parentNode;
     showNode(el);
- 
+
     el = el.querySelector('section > *');
     showNode(el);
 
 }
 
 function showNode(el) {
-    let travText = document.getElementById('basicWalk');
     let nodeType = el.nodeType;
     let nodeName = el.nodeName;
     let nodeValue = el.nodeValue;
 
-    travText.append(`Node type: ${nodeType}\nNode name: ${nodeName}\nNode value: ${nodeValue}\n\n`);
+    document.getElementById('travTxt').append(`Node type: ${nodeType}\nNode name: ${nodeName}\nNode value: ${nodeValue}\n\n`);
 }
 
 function advWalk() {
-    // in progress
+    /*let walker = document.createTreeWalker();
+    let el;
+
+    el = walker.root;
+    showName(el);
+    alert(el.nodeName);*/
+}
+
+function showName(el) {
+    let nodeName = el.nodeName;
+    document.getElementById('travTxt').append(`Node name: ${nodeName}\n`);
 }
 
 function modify() {
@@ -83,6 +122,19 @@ function modify() {
     // you can also update the dataset which change data-* attributes
     el.dataset.cool = 'true';       // data-cool="true"
     el.dataset.coolFactor = '9000'; //data-cool-factor="9000"
+
+}
+
+function advModify(){
+    let el = document.getElementsByTagName('h1')[0];
+    let num = Math.ceil(Math.random() * 6);
+    el.textContent = 'DOM Manipulation is Fun!';
+    el.style.color = 'var(--darkcolor' + num + ')';
+
+    let tagElems = document.getElementsByTagName('p');
+    for (var i=0; i < tagElems.length; i++) {
+        tagElems[i].classList.toggle('shmancy');;
+    }
 
 }
 
@@ -112,8 +164,66 @@ function add() {
     // clearly short hands are pretty easy!
 }
 
+function advAdd() {
+    let elemType = document.getElementById('elemSelect').value;
+    let elemCont = document.getElementById('elemCont').value;
+
+    let printCont = `This is a new ${elemType} ` + Date().toLocaleString();
+    let newElem;
+    
+    if (elemCont.length != 0) {
+        printCont = elemCont;
+    }
+
+    switch (elemType) {
+        case 'Text Node':
+            newElem = document.createElement('p');
+            newElem.textContent = printCont;
+            document.getElementById('addField').appendChild(newElem);
+            document.getElementById('addField').lastElementChild.classList.add('output');
+            break;
+        case 'Comment':
+            newElem = document.createComment(printCont);
+            document.getElementById('addField').appendChild(newElem);
+            break;
+        case 'Element':
+            let tagType = document.getElementById('tagType').value;
+            newElem = document.createElement(tagType);
+            newElem.textContent = printCont;
+            document.getElementById('addField').appendChild(newElem);
+            document.getElementById('addField').lastElementChild.classList.add('output');
+            break;
+    }
+}
+
 function remove() {
-  document.body.removeChild(document.body.lastChild);
+    document.body.removeChild(document.body.lastChild);
+}
+
+function safeRemove() {
+    let cur = document.body.lastElementChild;
+
+    if (cur.id === 'controls') {
+        cur = cur.previousSibling;
+    }
+    console.log(cur);
+    document.body.removeChild(cur);
+}
+
+function selectiveRemove() {
+    let selectCSS = document.getElementById('selectCSS').value;
+    document.querySelectorAll(`${selectCSS}`).forEach(e => e.remove());
+}
+
+function clone() {
+    var p1Elem = document.getElementById('p1');
+    var clone = p1Elem.cloneNode(true);
+    console.log(clone);
+    document.getElementById('cloneField').appendChild(clone);
+}
+
+function advClone() {
+    console.log('advClone');
 }
 
 window.addEventListener('DOMContentLoaded', init);
